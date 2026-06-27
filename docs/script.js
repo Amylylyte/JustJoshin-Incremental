25086ad26b7df0c24f7ea1d61bea928aae658238
Amylylyte <lushymid@gmail.com>
2026-06-27 15:58:17 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -367,7 +367,7 @@ function OvertapReset() {
     U2BOUGHT_ = 0;
     U3BOUGHT_ = 0;
     hardestFC = 0;
-    clicks = 0;
+    clicks = 1;
     notesHitPerSecond = new Decimal(1);
     notesHit = new Decimal(0);
     OvertapPoints = OvertapPoints.plus(pendingOvertap);
