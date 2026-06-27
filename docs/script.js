1069828a9359149b6748cc46a6c6cc83ac120888
Amylylyte <lushymid@gmail.com>
2026-06-27 15:19:15 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -259,7 +259,7 @@ function updateOvertapUpgradeButtons() {
         overtapUpgrade5Button.disabled = true;
         overtapUpgrade5Text.textContent =
             "Currently: ^" + formatNumber(OU5Boost);
-        OU5Boost = new Decimal(1).times((((new Decimal(OvertapPoints).plus(10)).log10()).log2()));
+        OU5Boost = new Decimal(5).minus(((4).times(new Decimal(0.99).pow((OvertapPoints).log10()))));
     }
     if (OU6Purchased == 1) {
         overtapUpgrade6Button.style.backgroundColor = "#95e347";
